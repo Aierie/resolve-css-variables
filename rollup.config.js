93fa6f5a17a96df845110227ef0be48bdf4d7f90
Aierie <hockeu.w@gmail.com>
2021-05-29 21:29:40 +0800

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist'
+  },
+  plugins: [typescript(), commonjs()],
+};
